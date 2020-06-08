2998cfa6f10dc922dccf7f23ed876c4d3aef62ac
Julian König <julian.koenig@js-soft.com>
2020-06-08 10:58:31 +0200

    feat: add proxy

--- test-runner/src/server.ts
+++ test-runner/src/server.ts
@@ -1,7 +1,17 @@
 import * as express from "express"
+import path = require("path")
 const app = express()
 
 app.use("/test-browser", express.static(process.argv[2]))
 app.use("/test", express.static(process.argv[3]))
+
+const config: any = require(path.join(process.cwd(), "nbt.json"))
+if (config && config.proxies) {
+    const httpproxy = require("express-http-proxy")
+
+    for (const proxy of config.proxies) {
+        app.use(proxy.local, httpproxy(proxy.remote))
+    }
+}
 
 app.listen(7777, () => {})
