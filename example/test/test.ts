040a17c96691955c2d9a70ae077ca69a7af69f9a
jkoenig <julian.koenig@js-soft.com>
2022-08-23 15:28:07 +0200

    chore: decrease test time

--- example/test/test.ts
+++ example/test/test.ts
@@ -10,6 +10,6 @@ function sleep(ms: number = 500): Promise<void> {
 
 describe("Long running tests", function () {
     it("should do this", async function () {
-        await sleep(600000)
-    }).timeout(700000)
+        await sleep(6000)
+    }).timeout(7000)
 })
