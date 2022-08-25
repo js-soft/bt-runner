e4293e40eb27c833635b0206b358ef187de54d9c
jkoenig <julian.koenig@js-soft.com>
2022-08-25 08:08:03 +0200

    chore: move runTests out of index.ts

--- test-runner/src/index.ts
+++ test-runner/src/index.ts
@@ -7,7 +7,7 @@ import { hideBin } from "yargs/helpers"
 import yargs from "yargs/yargs"
 import generate from "./generate"
 import getPort from "./getPort"
-import { IRunner } from "./IRunner"
+import { runTests } from "./runTests"
 import rimraf = require("rimraf")
 
 async function run() {
@@ -49,61 +49,5 @@ async function run() {
 
     process.exit(exitCode)
 }
-
-async function runTests(serverPort: number, testRunners: IRunner[]) {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ["--disable-gpu", "--disable-dev-shm-usage", "--disable-setuid-sandbox", "--no-sandbox"]
-    })
-
-    const page = await browser.newPage()
-
-    page.on("console", (message) => {
-        console.log.apply(
-            console,
-            message.args().map((arg) => arg.remoteObject().value)
-        )
-    })
-
-    for (let i = 0; i < testRunners.length; i++) {
-        const runner = testRunners[i]
-        const runnerNumber = i + 1
-
-        console.log(`Running test runner ${runnerNumber}`)
-
-        await page.goto(`http://localhost:${serverPort}/test-browser/index${runnerNumber}.html`)
-
-        const globals = runner.globals ?? []
-        globals.push("mocha")
-
-        const result = await page.evaluate(async (globals) => {
-            for (const glob of globals) {
-                if (typeof window[glob] === "undefined") {
-                    console.log(`Required library '${glob}' not loaded. Aborting...`)
-                    return { failures: 1 }
-                }
-            }
-
-            return new Promise<{ failures: number }>((resolve) =>
-                (window as any).mocha.run(function (failures: number) {
-                    resolve({ failures: failures })
-                })
-            )
-        }, runner.globals ?? [])
-
-        if (result.failures != 0) {
-            await browser.close()
-            return false
-        }
-
-        console.log()
-        console.log()
-    }
-
-    await browser.close()
-    return true
-}
-
-function cleanup(httpServerProc: child_process.ChildProcess, tempFolder: string) {}
 
 run()
---
+++ test-runner/src/runTests.ts
@@ -0,0 +1,56 @@
+import puppeteer from "puppeteer"
+import { IRunner } from "./Config"
+
+export async function runTests(serverPort: number, testRunners: IRunner[]) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ["--disable-gpu", "--disable-dev-shm-usage", "--disable-setuid-sandbox", "--no-sandbox"]
+    })
+
+    const page = await browser.newPage()
+
+    page.on("console", (message) => {
+        console.log.apply(
+            console,
+            message.args().map((arg) => arg.remoteObject().value)
+        )
+    })
+
+    for (let i = 0; i < testRunners.length; i++) {
+        const runner = testRunners[i]
+        const runnerNumber = i + 1
+
+        console.log(`Running test runner ${runnerNumber}`)
+
+        await page.goto(`http://localhost:${serverPort}/test-browser/index${runnerNumber}.html`)
+
+        const globals = runner.globals ?? []
+        globals.push("mocha")
+
+        const result = await page.evaluate(async (globals) => {
+            for (const glob of globals) {
+                if (typeof window[glob] === "undefined") {
+                    console.log(`Required library '${glob}' not loaded. Aborting...`)
+                    return { failures: 1 }
+                }
+            }
+
+            return new Promise<{ failures: number }>((resolve) =>
+                (window as any).mocha.run(function (failures: number) {
+                    resolve({ failures: failures })
+                })
+            )
+        }, runner.globals ?? [])
+
+        if (result.failures != 0) {
+            await browser.close()
+            return false
+        }
+
+        console.log()
+        console.log()
+    }
+
+    await browser.close()
+    return true
+}
