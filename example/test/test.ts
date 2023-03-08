e17987a70fc8a46034a2886bfac4ee1dd1c081f4
jkoenig <julian.koenig@js-soft.com>
2023-03-08 16:14:52 +0100

    chore: update example

--- example/test/test.ts
+++ example/test/test.ts
@@ -13,3 +13,16 @@ describe("Long running tests", function () {
         await sleep(3000)
     }).timeout(7000)
 })
+
+describe("env var mounting", function () {
+    before(function () {
+        // simulate the bt-runner behaviour for nodejs
+        if (globalThis.process !== undefined) {
+            globalThis.TEST_VAR = process.env.TEST_VAR
+        }
+    })
+
+    it("should mount env vars", async function () {
+        if (globalThis.TEST_VAR != "test") throw new Error("TEST_VAR is not set to 'test'")
+    })
+})
