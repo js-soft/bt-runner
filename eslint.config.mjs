43b5998340fb26ceb83e4d4512660380e66ce9ee
Julian König <julian.koenig@js-soft.com>
2025-07-16 11:40:03 +0200

    chore: migrate

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+import { configs } from "@js-soft/eslint-config-ts"
+import { globalIgnores } from "eslint/config"
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(globalIgnores(["**/dist", "**/node_modules"]), {
+    extends: [configs.base],
+    languageOptions: {
+        parserOptions: {
+            project: ["./tsconfig.json"]
+        }
+    },
+    files: ["**/*.ts"],
+    rules: {
+        "no-console": "off"
+    }
+})
